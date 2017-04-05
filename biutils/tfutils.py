45cd941ec60bcdda6df2e6cf259e448fca902c6e
Thomas Unterthiner <unterthiner@bioinf.jku.at>
2017-04-05 15:47:31 +0200

    ELU op now has a name so it looks nicer in TensorBoard graphs

--- biutils/tfutils.py
+++ biutils/tfutils.py
@@ -468,9 +468,10 @@ class LSTMwithSummaries(tf.contrib.rnn.RNNCell):
 
 
 def selu(x):
-    alpha = 1.6732632423543772848170429916717
-    scale = 1.0507009873554804934193349852946
-    return scale*tf.where(x>=0.0, x, alpha*tf.nn.elu(x))
+    with ops.name_scope('elu') as scope:
+        alpha = 1.6732632423543772848170429916717
+        scale = 1.0507009873554804934193349852946
+        return scale*tf.where(x>=0.0, x, alpha*tf.nn.elu(x))
 
 
 import numbers
