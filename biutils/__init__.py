7bcae4cc6d5eecfaea88068368c4f9065cefd9c5
Thomas Unterthiner <unterthiner@bioinf.jku.at>
2017-06-06 13:43:33 +0200

    Import plot_tiles by default

--- biutils/__init__.py
+++ biutils/__init__.py
@@ -1,8 +1,8 @@
 from biutils.misc import *
 from biutils.math import *
 from biutils.datasets import load_dataset
+from biutils.plot import plot_tiles
 
 import pkg_resources as __pkg_resources
 __version__ = __pkg_resources.require('biutils')[0].version
 
-#all = ['neuralnet', 'util', 'layers', 'op']
